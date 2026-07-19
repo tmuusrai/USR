7dd337b3934a266d8ab50bcf1b005cc923ba380c
Kuo <jewelw712kd@gmail.com>
2026-07-19 21:15:31 +0800

    perf: set thinking_budget=1024 to test speed/quality tradeoff

--- app.py
+++ app.py
@@ -863,7 +863,7 @@ llm = ChatGoogleGenerativeAI(
     model=os.environ["LLM_MODEL"],
     google_api_key=GOOGLE_API_KEY,
     temperature=0.2,
-    thinking_budget=4096,
+    thinking_budget=1024,
 )
 
 vectorstores: dict = {}
